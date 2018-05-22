487ed48e709e32d02f7bf0173f32d961040f7cd9
Alexandar Mechev <apmechev@gmail.com>
2018-05-22 14:26:39 +0200

    Adding logo

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,7 @@ def setup(app):
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = 'img/logo.png'
 
 
 # -- Options for HTMLHelp output ------------------------------------------
