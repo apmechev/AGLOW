aaa1085adc2d507d0dc9bc5e4e25ff25010f65f7
Alexandar Mechev <apmechev@gmail.com>
2018-09-04 13:49:28 +0200

    update to RC2 and adding sshtunnel as prereq

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(name='AGLOW',
           'pytest', 
           ],
       install_requires=[
+          'sshtunnel',
           'snakebite',
           'tzlocal==1.5.1',   
           'apache-airflow',
