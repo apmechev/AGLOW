cc9f6ec9efb69cab4377f312c000b330e984d648
Alexandar Mechev <apmechev@gmail.com>
2018-10-08 13:03:07 +0200

    continuingdependencies woes

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(name='AGLOW',
           ],
       install_requires=[
           'flask-appbuilder==1.11.1',
+          'tzlocal<2.0.0.0,>=1.5.0.0',
           'apache-airflow',
           'GRID_LRT>=0.4.3.1',
           'six'
