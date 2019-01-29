11d80397068de7f790fd63a6fc3ee246821c74ef
Alexandar Mechev <apmechev@gmail.com>
2019-01-29 11:54:08 +0100

    updating patch for airflow v1.10.2 and CI Utils

--- AGLOW/airflow/utils/AGLOW_CI_utils.py
+++ AGLOW/airflow/utils/AGLOW_CI_utils.py
@@ -1,13 +1,20 @@
-
 import requests
 import datetime
+import pickle
 
 def get_singularity_image_date(container_ID='6500'):
-    image_data = requests.get("https://www.singularity-hub.org/api/containers/{}/".format(container_ID)
-            ).json()['files']
-    created_date = image_data[0]['timeCreated']
-    update_date = image_data[0]['updated']
-    name = image_data[0]['name']
+    """ Gets the latest date for a singularity container hosted on 
+    www.singularity-hub.org using their API. The containerID has to be 
+    determined through the website. 
+    """
+    image_files = requests.get("https://www.singularity-hub.org/api/containers/{}/"
+            .format(container_ID)).json()['files']
+    if len(image_files)==0:
+        print("WARNING: No image files exist, image probably building")
+        return None
+    created_date = image_files[0]['timeCreated']
+    update_date = image_files[0]['updated']
+    name = image_files[0]['name']
     created_datetime = datetime.datetime.strptime(created_date,
             "%Y-%m-%dT%H:%M:%S.%fZ")
     updated_datetime = datetime.datetime.strptime(update_date,
@@ -18,6 +25,10 @@ def get_singularity_image_date(container_ID='6500'):
 
 
 def get_git_repository_date(repo_user=None, repo_name=None, branch='master'):
+    """Uses the GitHub API to get the date of the latest commit for a repo for
+    a specific user. If no branch is selected, it defaults with 'master' branch
+
+    """
     r=requests.get("https://api.github.com/repos/{}/{}".format(repo_user,repo_name))
     repo = r.json()
     branches = requests.get("https://api.github.com/repos/{}/{}/branches".format(repo_user,repo_name)).json()
@@ -32,4 +43,6 @@ def get_git_repository_date(repo_user=None, repo_name=None, branch='master'):
     return commit_datetime
 
 
-
+def check_stored_date(savefile='/home/apmechev/.prefactor_v3.0_CI.pkl', key='lofar.simg'):
+    data = pickle.load(open(savefile,'rb'))
+    return data[key]
--- AGLOW/tests/test_patches_applied.py
+++ AGLOW/tests/test_patches_applied.py
@@ -1,8 +1,12 @@
 import airflow.www as www
+import airflow.__version__
 
 
 WWW_LOC = www.__file__.split('__init__.py')[0]
 TEMPLATES_LOC = "{}/templates/airflow".format(WWW_LOC)
+FILE_LENGTHS={'dag.html':{'1.10.0':436,
+                          '1.10.1':450,
+                          '1.10.2':529}
 
 def test_dag_patched():
     dagfile = TEMPLATES_LOC+"/dag.html"
